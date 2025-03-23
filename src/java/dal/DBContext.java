ed6a60b7c4b6604ce1bf786610e70c37e5b10d68
ThachNK <thachnk@gmail.com>
2025-03-23 20:13:14 +0700

    update

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -13,24 +13,11 @@ import java.util.logging.Logger;
  */
 public class DBContext {
 
-//    public Connection connection;
-//
-//    public DBContext() throws ClassNotFoundException {
-//        try {
-//            String user = "sa";
-//            String pass = "namnp";
-//            String url = "jdbc:sqlserver://localhost:1433;databaseName=SWP391_Project;encrypt=true;trustServerCertificate=true";
-//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//            connection = DriverManager.getConnection(url, user, pass);
-//        } catch (SQLException e) {
-//            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, e);
-//        }
-//    }
     public static Connection getConnection() {
         Connection connection = null;
         try {
             String user = "sa";
-            String pass = "Kimanh3082@";
+            String pass = "123";
             String url = "jdbc:sqlserver://localhost:1433;databaseName=SWP391_Re;encrypt=true;trustServerCertificate=true";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
